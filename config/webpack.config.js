ac3a91d43bb299235082371f247204e44c797cd3
NriotHrreion <nriot233@gmail.com>
2022-08-02 19:31:02 +0800

    ci: Critical dependency: the request of a dependency is an expression

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -366,6 +366,7 @@ module.exports = function (webpackEnv) {
     },
     module: {
       strictExportPresence: true,
+      exprContextCritical: false,
       rules: [
         // Disable require.ensure as it's not a standard language feature.
         { parser: { requireEnsure: false } },
