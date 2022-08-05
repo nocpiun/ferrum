e53ac5250a0885a6b6502dcd4b483ba813446e8c
NriotHrreion <nriot233@outlook.com>
2022-08-05 11:10:01 +0800

    i18n: Add zh-TW and en
    Co-authored-by: Deed <henlowornd@users.noreply.github.com>
    Co-authored-by: NriotHrreion <nriot233@outlook.com>

--- src/lang/index.tsx
+++ src/lang/index.tsx
@@ -1,5 +1,9 @@
 import zh_CN from "./zh-CN.json";
+import zh_TW from "./zh-TW.json";
+import en from "./en.json";
 
 export default {
-    "zh-CN": zh_CN
+    "zh-CN": zh_CN,
+    "zh-TW": zh_TW,
+    "en": en,
 };
