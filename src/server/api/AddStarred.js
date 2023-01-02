391210e23bdf6398197c878178802590a3e114cb
NriotHrreion <nriot233@outlook.com>
2023-01-02 14:06:10 +0800

    fix: Star list incorrect order

--- src/server/api/AddStarred.js
+++ src/server/api/AddStarred.js
@@ -6,7 +6,8 @@ const starsPath = path.join(__dirname, "../stars.json");
 module.exports = function(req, res) {
     var newStarDir = req.body.path;
     var list = new Map(JSON.parse(fs.readFileSync(starsPath)));
-    list.set(list.size, newStarDir);
+
+    list.set(newStarDir, newStarDir);
 
     fs.writeFileSync(starsPath, JSON.stringify(Array.from(list)));
     res.end();
