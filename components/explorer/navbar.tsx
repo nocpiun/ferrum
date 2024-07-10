de72db505737d2503b218bf7d0f077a747149b98
NriotHrreion <nriot233@outlook.com>
2024-07-10 22:19:41 +0800

    feat: Add folder icons for explorer navbar

--- components/explorer/navbar.tsx
+++ components/explorer/navbar.tsx
@@ -1,19 +1,66 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/breadcrumbs";
+import { to } from "preps";
+import {
+    Folder,
+    FolderRoot,
+    FolderGit2,
+    FolderDot,
+    FolderHeart,
+    Music,
+    Images,
+    Film
+} from "lucide-react";
 
 import { useExplorer } from "@/hooks/useExplorer";
 import { PropsWithCN } from "@/types";
 
+function getIcon(index: number, folderName: string): React.ReactNode {
+    const folderNameLowered = folderName.toLowerCase();
+    const size = 18;
+
+    if(index === 0) return <FolderRoot size={size}/>;
+    if(folderNameLowered === ".git") return <FolderGit2 size={size}/>;
+    if(/^\.\S+/.test(folderNameLowered)) return <FolderDot size={size}/>;
+    if(/^(favo(u?)rite(s?))$/.test(folderNameLowered)) return <FolderHeart size={size}/>;
+    if(/^(music|song(s?))$/.test(folderNameLowered)) return <Music size={size}/>;
+    if(/^(picture(s?)|image(s?))$/.test(folderNameLowered)) return <Images size={size}/>;
+    if(/^(video(s?)|film(s?)|movie(s?))$/.test(folderNameLowered)) return <Film size={size}/>;
+    
+    return <Folder size={size}/>;
+}
+
 const Navbar: React.FC<PropsWithCN> = ({ className }) => {
+    const router = useRouter();
     const explorer = useExplorer();
 
+    const handleClick = (index: number) => {
+        const { path } = explorer;
+
+        explorer.setPath(to(path).cut(index + 1).f(0));
+        router.push("/x/root"+ explorer.stringifyPath());
+    };
+
     return (
-        <Breadcrumbs className={className}>
+        <Breadcrumbs
+            className={className}
+            maxItems={15}
+            itemsBeforeCollapse={3}
+            itemsAfterCollapse={5}>
             {
-                explorer.path.map((value, index) => (
-                    <BreadcrumbItem key={index}>{value}</BreadcrumbItem>
+                explorer.path.map((folderName, index, { length }) => (
+                    <BreadcrumbItem
+                        classNames={{ item: "gap-1" }}
+                        isCurrent={index !== 0 && index === length - 1}
+                        isDisabled={index === 0 && length === 1}
+                        onClick={() => handleClick(index)}
+                        key={index}>
+                        {getIcon(index, folderName)}
+                        {folderName}
+                    </BreadcrumbItem>
                 ))
             }
         </Breadcrumbs>
